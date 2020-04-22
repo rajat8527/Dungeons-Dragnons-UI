e9a072c085ee6ae0253b78014fcbb7c2bf299a3a
Rajat Meena <rajat8527@gmail.com>
2020-04-22 08:05:48 +0530

    delete button added

--- server.js
+++ server.js
@@ -4,17 +4,7 @@ const path = require('path');
 var cors = require('cors');
 const port = process.env.PORT || 8080;
 const app = express();
-var whitelist = ['https://rakuten-dnd-character-app.herokuapp.com/api/getCharacterData','https://rakuten-dnd-character-app.herokuapp.com/api/saveCharacterData', 'https://rakuten-dnd-character-app.herokuapp.com/api/deleteAllData']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
