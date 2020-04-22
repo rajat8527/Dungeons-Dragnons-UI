3cbe2869851aab28813d5bccac6900631c80bdb1
Rajat Meena <rajat8527@gmail.com>
2020-04-22 21:10:38 +0530

    delete character by id added

--- src/CharacterListView.js
+++ src/CharacterListView.js
@@ -124,7 +124,7 @@ class CharacterListView extends React.Component {
                         <div className="w3-col w3-padding l4 w3-right">
                           <div className="w3-bar">
                             <button onClick={() => { this.dataTransferview(iter) }} className="w3-button w3-padding w3-round-xxlarge w3-highway-red w3-hover-red" ><b>More Info</b></button>
-                            <button onClick={() => { this.deleteCharacterById(iter._id,index) }} className="delete-button w3-button w3-padding w3-round-xxlarge w3-highway-red w3-hover-red" >{this.state.loadDeleteById ? <FontAwesomeIcon spin icon={faSpinner} /> : 'Delete'}</button>
+                            <button onClick={() => { this.deleteCharacterById(iter._id,index) }} className="delete-button w3-button w3-padding w3-round-xxlarge w3-highway-red w3-hover-red" ><b>Delete</b></button>
                           </div>
                         </div>
                       </div>
